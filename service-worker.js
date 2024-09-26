439500cad33014bcca708476a58f205ce838c920
Yatish Sekaran <yatish.cs17@bmsce.ac.in>
2024-09-25 23:42:25 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/proj1/precache-manifest.00659e0fc46060e6ce0589cc0d2ef2a1.js"
+  "/proj1/precache-manifest.f25a1b7770d1f056e8c485c4e00f7612.js"
 );
 
 self.addEventListener('message', (event) => {
