898a1c88d9896eaa2d1cc52a7294d0f1e12f311c
Yatish Sekaran <yatish.cs17@bmsce.ac.in>
2024-09-26 00:14:44 -0700

    Updates

--- precache-manifest.4bce34d3754304cded433a00dae8f88e.js
+++ precache-manifest.4bce34d3754304cded433a00dae8f88e.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "2ddd288ccc0598b03f15578a2b6d4db6",
+    "revision": "77766bb3ddfe47fea81afa8b1f67d450",
     "url": "/proj1/index.html"
   },
   {
-    "revision": "58a3bd1bc6235c23edf3",
+    "revision": "5c6384bf9422b7e97f4a",
     "url": "/proj1/static/css/main.435d5fe5.chunk.css"
   },
   {
-    "revision": "fb25ae7a37d196e17789",
-    "url": "/proj1/static/js/2.3bea8d94.chunk.js"
+    "revision": "23d1d5bb535b218f19fa",
+    "url": "/proj1/static/js/2.46f87596.chunk.js"
   },
   {
     "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
-    "url": "/proj1/static/js/2.3bea8d94.chunk.js.LICENSE.txt"
+    "url": "/proj1/static/js/2.46f87596.chunk.js.LICENSE.txt"
   },
   {
     "revision": "3d9e6bf7f5e4429ebf36",
     "url": "/proj1/static/js/3.adc7235d.chunk.js"
   },
   {
-    "revision": "58a3bd1bc6235c23edf3",
-    "url": "/proj1/static/js/main.1d84d185.chunk.js"
+    "revision": "5c6384bf9422b7e97f4a",
+    "url": "/proj1/static/js/main.33a5a22e.chunk.js"
   },
   {
     "revision": "651bc7970f89c81484bb",
