21299453add1042378c19a2579e3928ef4bd3b22
Yatish Sekaran <yatish.cs17@bmsce.ac.in>
2024-09-25 18:41:58 -0700

    Updates

---
+++ precache-manifest.0c17f7ac532ab431e190e88b74780980.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eee655f05292431d948710c6466c7391",
+    "url": "/index.html"
+  },
+  {
+    "revision": "58a3bd1bc6235c23edf3",
+    "url": "/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "fb25ae7a37d196e17789",
+    "url": "/static/js/2.3bea8d94.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/static/js/2.3bea8d94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d9e6bf7f5e4429ebf36",
+    "url": "/static/js/3.adc7235d.chunk.js"
+  },
+  {
+    "revision": "58a3bd1bc6235c23edf3",
+    "url": "/static/js/main.1d84d185.chunk.js"
+  },
+  {
+    "revision": "9b7ecfbfd1c5fb04bab6",
+    "url": "/static/js/runtime-main.c6811341.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/static/media/logo.06e73328.svg"
+  }
+]);
