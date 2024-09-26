cf370dae8f4bf7b4ff129940d723439f703535f9
Yatish Sekaran <yatish.cs17@bmsce.ac.in>
2024-09-25 19:28:18 -0700

    Updates

---
+++ precache-manifest.658fceddbbe35f4d5169ee94ce769b83.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac9da4cfff9f07a8326bd1f5e6375e35",
+    "url": "/proj1/index.html"
+  },
+  {
+    "revision": "58a3bd1bc6235c23edf3",
+    "url": "/proj1/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "fb25ae7a37d196e17789",
+    "url": "/proj1/static/js/2.3bea8d94.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/proj1/static/js/2.3bea8d94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d9e6bf7f5e4429ebf36",
+    "url": "/proj1/static/js/3.adc7235d.chunk.js"
+  },
+  {
+    "revision": "58a3bd1bc6235c23edf3",
+    "url": "/proj1/static/js/main.1d84d185.chunk.js"
+  },
+  {
+    "revision": "651bc7970f89c81484bb",
+    "url": "/proj1/static/js/runtime-main.9f3ae02a.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/proj1/static/media/logo.06e73328.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.207f18f8d9a54e241fec79cec1d9d160.js"
+  "/proj1/precache-manifest.658fceddbbe35f4d5169ee94ce769b83.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/proj1/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
